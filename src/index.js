5fd080da350eed7dbbbbda1dcd20cd1b26de1de5
Glayson Visgueira <glaysonwow@gmail.com>
2023-06-23 16:43:12 -0300

    Primeiro commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import http from "http";
+import express from "express";
+import cors from "cors";
+import { Router } from "express";
+
+const PORT = 3001;
+
+const router = Router();
+const server = express();
+server.use(
+  cors({
+    origin: "*",
+  })
+);
+
+server.use(express.json());
+server.use(router);
+
+http
+  .createServer(server)
+  .listen(PORT, () => console.log(`SERVER IS RUNNING ON PORT ${PORT}.`));
+
+server.get("/", (req, res) => {
+  return res.json({ message: "hello world" });
+});
